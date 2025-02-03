c430abf3450e820eb32eee3a13a1f422846131bc
Damon P. Cortesi <d.lifehacker@gmail.com>
2025-02-03 15:06:59 -0800

    Initial revision of HA Storybutton integration

---
+++ custom_components/storybutton/media_player.py
@@ -0,0 +1,157 @@
+"""Platform for Storybutton integration."""
+
+import logging
+
+from homeassistant.components.media_player import MediaPlayerEntity
+from homeassistant.components.media_player.const import MediaPlayerEntityFeature
+from homeassistant.const import (
+    CONF_HOST,
+    CONF_NAME,
+    STATE_IDLE,
+    STATE_OFF,
+    STATE_ON,
+    STATE_PAUSED,
+    STATE_PLAYING,
+)
+from homeassistant.core import HomeAssistant
+from homeassistant.helpers.typing import ConfigType
+
+from custom_components.storybutton.storybutton import State, Storybutton
+
+_LOGGER = logging.getLogger(__name__)
+
+
+async def async_setup_platform(hass, config, async_add_entities, discovery_info=None):
+    """Set up the storybutton platform."""
+    # devices = [
+    #     MyMediaPlayer("Device 1", host="192.168.1.10"),
+    #     MyMediaPlayer("Device 2", host="192.168.1.11"),
+    # ]
+    host = config.get(CONF_HOST)
+    name = config.get(CONF_NAME)
+    _LOGGER.info(f"Setting up Storybutton {name}@{host}")
+    if host is None:
+        return
+
+    device = StoryButtonEntity(hass, host, name)
+
+    async_add_entities([device], update_before_add=True)
+
+
+class StoryButtonEntity(MediaPlayerEntity):
+    """Representation of a Storybutton that communicates with your local device."""
+
+    def __init__(self, hass, host, name):
+        """Initialize the media player."""
+        self.hass = hass  # Store hass reference
+        self._name = name
+        self._host = host
+        self._state = STATE_OFF
+        self._attr_volume_level = 0
+        self._supported_features = (
+            MediaPlayerEntityFeature.PLAY
+            | MediaPlayerEntityFeature.PAUSE
+            | MediaPlayerEntityFeature.VOLUME_STEP
+            | MediaPlayerEntityFeature.VOLUME_SET
+            | MediaPlayerEntityFeature.VOLUME_MUTE
+        )
+
+        self.sb_device = Storybutton(self._host)
+
+    @property
+    def name(self):
+        """Return the name of the media player."""
+        return self._name
+
+    @property
+    def state(self):
+        """Return the state of the media player."""
+        return self._state
+
+    @property
+    def supported_features(self):
+        """Flag media player features that are supported."""
+        return self._supported_features
+
+    async def async_update(self):
+        """Fetch new state data for the media player.
+
+
+        This is where you can query your device using REST (or other protocols)
+        and update the internal state accordingly.
+        """
+        # Example (pseudo-code):
+        # response = await your_async_http_get(f"http://{self._host}/status")
+        # self._state = parse_state_from_response(response)
+        _LOGGER.debug("Updating state for %s", self._name)
+        # For now, we’ll just simulate a state update:
+        self._state = STATE_IDLE
+
+        if not await self.hass.async_add_executor_job(self.sb_device.get_power_status):
+            self._state = STATE_OFF
+            return
+
+        self._state = STATE_ON
+
+        # Retrieve latest data
+        try:
+            await self.async_update_volume()
+            if self._name is None:
+                self._name = self.sb_device.name()
+            status = await self.hass.async_add_executor_job(self.sb_device.status)
+            if status == State.PLAYING:
+                self._state = STATE_PLAYING
+            elif status == State.PAUSED:
+                self._state = STATE_PAUSED
+        except Exception as exception_instance:  # pylint: disable=broad-except
+            # TODO: Remove this or handle the NewConnectionError(urllib3.connection.HTTPConnection object) as this may be expected
+            _LOGGER.error(exception_instance)
+            self._state = STATE_OFF
+
+    async def async_media_play(self):
+        """Send play command to media player."""
+        await self.hass.async_add_executor_job(self.sb_device.play)
+        self._state = STATE_PLAYING
+
+    async def async_media_pause(self):
+        """Send pause command to media player."""
+        await self.hass.async_add_executor_job(self.sb_device.pause)
+        self._state = STATE_PAUSED
+
+    async def async_media_stop(self):
+        """Send stop command to media player."""
+        await self.hass.async_add_executor_job(self.sb_device.pause)
+        self._state = STATE_IDLE
+
+    async def async_update_volume(self):
+        self._attr_volume_level = (
+            await self.hass.async_add_executor_job(self.sb_device.get_volume)
+        ) / 100
+        _LOGGER.debug(self._attr_volume_level)
+
+    async def async_volume_up(self):
+        """Send stop command."""
+        self._attr_volume_level = await self.hass.async_add_executor_job(
+            self.sb_device.volume_up
+        )
+
+    async def async_volume_down(self):
+        """Send stop command."""
+        self._attr_volume_level = await self.hass.async_add_executor_job(
+            self.sb_device.volume_down
+        )
+
+    async def async_set_volume_level(self, volume):
+        """Send set volume command."""
+        await self.hass.async_add_executor_job(
+            self.sb_device.set_volume, int(volume * 100)
+        )
+
+    async def async_mute_volume(self, mute):
+        """Send mute command."""
+        if mute:
+            await self.hass.async_add_executor_job(self.sb_device.mute)
+            self._attr_is_volume_muted = True
+        else:
+            await self.hass.async_add_executor_job(self.sb_device.unmute)
+            self._attr_is_volume_muted = False
---
+++ custom_components/storybutton/storybutton.py
@@ -0,0 +1,90 @@
+from enum import Enum
+import upnpclient
+import requests
+
+class State(Enum):
+    UNKNOWN = 0
+    OFF = 1
+    ON = 2
+    PLAYING = 3
+    PAUSED = 4
+
+class Storybutton:
+    """A utility to interact with the Storybutton"""
+    def __init__(self, host: str) -> None:
+        self._host = host
+        self._endpoint = f"http://{host}"
+
+        # We don't initialize this here b/c it makes a sync call and HA gets mad
+        self._upnp_client = None
+    
+    @property
+    def upnp_client(self):
+        if not self._upnp_client:
+            self._upnp_client = upnpclient.Device(f"{self._endpoint}/device.xml")
+        
+        return self._upnp_client
+    
+    def get_power_status(self) -> bool:
+        try:
+            requests.get(self._endpoint, timeout=3)
+            return True
+        except (requests.exceptions.Timeout, requests.exceptions.ConnectionError):
+            return False
+    
+    def status(self) -> State:
+        """Query the device for its status, if none, it's off"""
+        if not self.get_power_status():
+            return State.OFF
+
+        # The device is up, so get the transport info
+        resp = self.upnp_client.AVTransport.GetTransportInfo(InstanceID=0)
+        current_state = resp.get('CurrentTransportState', None)
+        if current_state == 'PAUSED_PLAYBACK':
+            return State.PAUSED
+        elif current_state == 'PLAYING':
+            return State.PLAYING
+        else:
+            return State.UNKNOWN
+    
+    def name(self):
+        return self.upnp_client.friendly_name
+    
+    def volume_up(self) -> int:
+        current_volume = self.get_volume()
+        if current_volume < 100:
+            self.upnp_client.RenderingControl.SetVolume(InstanceID=0, Channel='Master', DesiredVolume=current_volume+1)
+            return current_volume+1
+        else:
+            return current_volume
+    
+    def volume_down(self) -> int:
+        current_volume = self.get_volume()
+        if current_volume > 0:
+            self.upnp_client.RenderingControl.SetVolume(InstanceID=0, Channel='Master', DesiredVolume=current_volume-1)
+            return current_volume-1
+        else:
+            return current_volume
+    
+    def get_volume(self) -> int:
+        """Returns the current volume, a value between 0 and 100"""
+        resp = self.upnp_client.RenderingControl.GetVolume(InstanceID=0,Channel="Master")
+        if 'CurrentVolume' not in resp:
+            raise Exception("No volume returned from device: ", resp)
+        
+        return resp.get('CurrentVolume')
+
+    def set_volume(self, desired_volume: int):
+        self.upnp_client.RenderingControl.SetVolume(InstanceID=0, Channel='Master', DesiredVolume=desired_volume)
+
+    def play(self):
+        self.upnp_client.AVTransport.Play(InstanceID=0, Speed='1')
+
+    def pause(self):
+        self.upnp_client.AVTransport.Pause(InstanceID=0)
+    
+    def mute(self):
+        self.upnp_client.RenderingControl.SetMute(InstanceID=0, Channel='Master', DesiredMute="1")
+
+    def unmute(self):
+        self.upnp_client.RenderingControl.SetMute(InstanceID=0, Channel='Master', DesiredMute="0")
